f007c7a3658ea218009ccc752f2e257fbf878f3d
raelaraujo0 <raellyedson1@gmail.com>
2024-10-17 08:59:17 -0300

    add post, mongo db e async await no get e post

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express, { json, request, response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express()
+const prisma = new PrismaClient()
+ 
 app.use(express.json())
 
-const books = []
+app.post('/books', async  (request, response) => {
+    await prisma.book.create({
+        data: {
+            author: request.body.author,
+            title: request.body.title,
+            releaseYear: request.body.releaseYear
+        }
+    })
 
-app.post('/books', (request, response) => {
-    books.push(request.body)
     response.status(201).json(request.body)
 })
 
-app.get('/books', (request, response) => {
+app.get('/books', async (request, response) => {
+    const books  = await prisma.book.findMany()
+
     response.status(200).json(books)
     response.send('Olar, esse é o inicio do projeto')
 })
