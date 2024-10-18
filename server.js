85fc4e08b515351f7892099d502dbc218afaaaad
raelaraujo0 <raellyedson1@gmail.com>
2024-10-18 09:34:27 -0300

    adicao PUT e UPDATE

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const prisma = new PrismaClient()
  
 app.use(express.json())
 
+app.get('/books', async (request, response) => {
+    let books = []
+
+    if(request.query){
+        books = await prisma.book.findMany({
+            where: {
+                author: request.query.author,
+                title: request.query.title,
+                releaseYear: request.query.releaseYear
+            }
+        })
+    } else {
+        books  = await prisma.book.findMany()
+    }
+
+    response.status(200).json(books)
+    response.send('Olar, esse é o inicio do projeto')
+})
+
 app.post('/books', async  (request, response) => {
     await prisma.book.create({
         data: {
@@ -18,11 +37,30 @@ app.post('/books', async  (request, response) => {
     response.status(201).json(request.body)
 })
 
-app.get('/books', async (request, response) => {
-    const books  = await prisma.book.findMany()
+app.put('/books/:id', async  (request, response) => {
+    await prisma.book.update({
+        where: {
+            id: request.params.id
+        },
 
-    response.status(200).json(books)
-    response.send('Olar, esse é o inicio do projeto')
+        data: {
+            author: request.body.author,
+            title: request.body.title,
+            releaseYear: request.body.releaseYear
+        }
+    })
+
+    response.status(201).json(request.body)
+})
+
+app.delete('/books/:id', async (request, response) => {
+    await prisma.book.delete({
+        where: {
+            id: request.params.id
+        }
+    })
+
+    response.status(200).json({message: 'O the book has been deleted'})
 })
 
 app.listen(3000)
